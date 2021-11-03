df2e24425b353fe3cca0f38b788ddefcc6a9d5dd
daroGH <chandaro37@gmail.com>
2021-11-04 10:05:19 +1030

    react dynamic menu project

---
+++ src/Categories.js
@@ -0,0 +1,22 @@
+import React from "react";
+//when the list extend it will automatically expend the tab 
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
